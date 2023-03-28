74dd57c7276766324311ab6d0653c89de3fa9822
tsering651 <tseringwang4039@gmail.com>
2023-03-28 10:39:34 +0530

    Creating a cart-system

---
+++ src/test/java/com/example/CartSystem/CartSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CartSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
